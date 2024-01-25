4895fce42859a052232f1390356f3fa7f8eca272
Aaron Francis <aaronandrew9@gmail.com>
2024-01-24 19:47:21 -0500

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,150 @@
+const logotext = "Welcome!";
+const meta = {
+    title: "Aaron Francis",
+    description: "I am a prominent Software Engineer as well as a passionate entrepreneur trying to dive into what's new with tech and keep ahead of the ever changing technical world. I've had experience with various fortune 500 companies(Bloomberg LP , Infosys), diving into software development, automation testing and cloud computing. I received an outstanding leadership award  for my leadership and operational skills from being president of multiple collegiate organizations during my time at A&T. Lastly I am also certified in Selenium automation testing , Java/DBMS and Agile development.",
+};
+
+const introdata = {
+    title: "I’m Aaron Francis",
+    animated: {
+        first: "Software Engineer",
+        second: "Web/App Developer",
+        third: "Entrepreneur",
+    },
+    description: "Welcome to my portfolio",
+   
+    
+};
+
+const dataabout = {
+    title: "Get to know me",
+    aboutme: "Embarking on my academic journey at North Carolina Agricultural and Technical State University (NCAT), I've passionately explored entrepreneurship, fostered innovation, and delved into the intricate relationship between humans and everyday technology. This journey has sculpted my skills, guiding me towards becoming a versatile professional in the realm of computer science and technology. My academic evolution began with a transition from mechanical to computer engineering at NCAT, where hackathons and engineering challenges affirmed my passion for computer science. As my interests shifted toward UI/UX design, courses like Game Design, Software Engineering & Design, and Information Security played a pivotal role in shaping my technical prowess and nurturing a keen eye for crafting secure and user-friendly applications. In 2019, my journey into research and entrepreneurship commenced with the initiation of website development for clients, leading to the establishment of Techpressers. This coding team, with expertise in cybersecurity, network engineering, and front-end design, marked my initial venture into a business-to-business structure, setting the stage for future endeavors.",
+};
+const worktimeline = [{
+        jobtitle: "Infosys",
+        where: "remote/NYC",
+        date: "Associate Software Engineer, June 2022 - present",
+    },
+    {
+        jobtitle: "TECHPRESSERS",
+        where: "Greensboro, NC",
+        date: "Web/App developer, Dec 2020 - June 2022",
+    },
+    {
+        jobtitle: "BLOOMBERG BETA, HBCU VENTURE CAPITAL",
+        where: "remote/NYC",
+        date: "Intern, Jun 2021 - Aug 2021",
+    },
+    {
+        jobtitle: "BLOOMBERG LP, ENGINEERING",
+        where: "remote/NYC",
+        date: "Accelerator, October 2021 - January 2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 60,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Mui",
+        value: 85,
+    },
+    {
+        name: "Next.js",
+        value: 85,
+    },
+    {
+        name: "AWS Amplify",
+        value: 60,
+    },
+    {
+        name: "Mongodb",
+        value: 65,
+    },
+    {
+        name: "Firebase",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Website/platforms",
+        description: "E-commerce or CRM platforms using visual studio ide and aws amplify for hosting and analytics to maintain your site.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Using flutter sdk to create cross platforms for ios and android using android studio as the ide.",
+    },
+    {
+        title: "Figma Design",
+        description: "App/Website design using figma that can be prototyped and turned into frontend code without functionality.",
+    },
+];
+
+const dataportfolio = [{
+        img: "one.png",
+        description: "Gourmand purpose is to empower Private Chefs everywhere, at all levels. Create unique experiences through fine dining.",
+        link: "https://github.com/Aaronandrew/thegourmandapp",
+    },
+    {
+        img: "four.png",
+        description: "Global Skater community social app",
+        link: "https://github.com/Aaronandrew/Global8",
+    },
+    {
+        img: "two.png",
+        description: "Powered by AI & Data, CantusIQ offers a comprehensive set of tools to manage, automate, and grow your business..",
+        link: "https://www.1601lab.com/",
+    },
+    {
+        img: "three.png",
+        description: "Dedicated to driving the evolution of work, media, and the creator economy by harnessing the power of cutting-edge technologies like Web3, AI, and IoT.",
+        link: "https://www.cantusiq.com/",
+    },
+    {
+        img: "five.png",
+        description: "Modal multiform for profile selection.",
+        link: "https://github.com/Aaronandrew/Mui-form",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aaronandrew9@gmail.com",
+    YOUR_FONE: "(347)456-8594",
+    description: "Looking to connect and discuss whats going on in the world of technology today. leave me a message. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_6023cbb",
+    YOUR_TEMPLATE_ID: "template_8wrrryf",
+    YOUR_USER_ID: "ViZ1wrewGBGp5djuy",
+};
+
+const socialprofils = {
+    github: "https://github.com/Aaronandrew",
+    linkedin: "https://www.linkedin.com/in/aaron-francis-30b55b175/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
